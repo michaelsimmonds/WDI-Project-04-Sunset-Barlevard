42a5527f7a04e38169e0980c6e2b4c9d8bc5195d
laceswingybethler <beth.swingler@gmail.com>
2019-02-27 15:29:53 +0000

    fixed tabs

--- seeds.py
+++ seeds.py
@@ -21,40 +21,40 @@ with app.app_context():
         raise Exception(errors)
     db.session.add(mike)
 
-# ======================================================BARS===========================================================
+# ==============================BARS==========================================
 
     jujus = Bar(
-    	name='Jujus',
-    	address='Ely\'s Yard, 15 Hanbury St, London E1 6QR',
-    	lat=51.520886,
-    	lng=-0.073487,
-    	terrace=True,
-    	description='We go all the time on Firdays'
+        name='Jujus',
+        address='Ely\'s Yard, 15 Hanbury St, London E1 6QR',
+        lat=51.520886,
+        lng=-0.073487,
+        terrace=True,
+        description='We go all the time on Firdays'
     )
     db.session.add(jujus)
 
     the_culpeper = Bar(
-    	name='The Culpeper',
-    	address='40 Commercial St, London E1 6LP',
-    	lat=51.5168921,
-    	lng=-0.0730285,
-    	terrace=True,
-    	description='Small but great roofterrace'
+        name='The Culpeper',
+        address='40 Commercial St, London E1 6LP',
+        lat=51.5168921,
+        lng=-0.0730285,
+        terrace=True,
+        description='Small but great roofterrace'
     )
     db.session.add(the_culpeper)
 
     discount_suit_company = Bar(
-    	name='Discount Suit Company',
-    	address='29 Wentworth St, London E1 7TB',
-    	lat=51.5166773,
-    	lng=0.0774967,
-    	terrace=False,
-    	description='Basement no light'
+        name='Discount Suit Company',
+        address='29 Wentworth St, London E1 7TB',
+        lat=51.5166773,
+        lng=0.0774967,
+        terrace=False,
+        description='Basement no light'
     )
 
     db.session.add(discount_suit_company)
 
-# =====================================================CRAWLS==========================================================
+# =========================CRAWLS=============================================
 
     east_end = Crawl(
         name='East End Crawl',
