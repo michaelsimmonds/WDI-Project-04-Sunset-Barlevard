621c092b013782aa5a7af22658edcb036b16dfc3
Michael Simmonds <michaelsimmonds92@hotmail.com>
2019-03-02 14:06:02 +0000

    Getting ready to merge

--- src/components/bars/BarsIndex.js
+++ src/components/bars/BarsIndex.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import axios from 'axios'
-import BarCard from './BarCard.js'
 
+import BarCard from './BarCard.js'
 
 class BarIndex extends React.Component{
   constructor(){
@@ -19,18 +19,17 @@ class BarIndex extends React.Component{
   }
 
   render(){
-<<<<<<< HEAD
-
-=======
->>>>>>> development
     if(!this.state.bars) return null
     console.log(this.state.bars)
     return(
 
       <section className="section">
         <h2 className="title is-1 has-text-centered">Discover our Bars and Pubs</h2>
+
+
         <div className="is-full-width">
           <hr />
+
           <h1 className="title is-2 has-text-centered">East London</h1>
           <div className="columns is-multiline">
             {this.state.bars.map(bar =>
