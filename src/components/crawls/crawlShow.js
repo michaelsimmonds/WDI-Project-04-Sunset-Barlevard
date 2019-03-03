407b3ed39b3cda6c1ed7f10d0df0cfb804c52782
laceswingybethler <beth.swingler@gmail.com>
2019-03-03 17:22:16 +0000

    Made function for averaging map center

--- src/components/crawls/crawlShow.js
+++ src/components/crawls/crawlShow.js
@@ -8,9 +8,8 @@ class CrawlShow extends React.Component {
     super()
 
     this.state={
-
+      zoomCenter: [-0.1293555, 51.546483]
     }
-
     //binds here
   }
 
@@ -18,27 +17,43 @@ class CrawlShow extends React.Component {
     axios.get(`/api/crawls/${this.props.match.params.id}`)
       .then(res => {
         this.setState({ crawl: res.data })
+        const stops = this.state.crawl.stops
+        console.log("stops: " + stops)
+        return stops
       })
+      .then(stops => {
+        //map over each stop and return just the lngs
+        const lngs = stops.map(stop => stop.bar.lng)
+        console.log('LNGS', lngs)
+        //map over each stop and return just the lats
+        const lats = stops.map(stop => stop.bar.lat)
+        console.log('LATS', lats)
+        //reduce the lngs and divide by lngs.length
+        const avLng = lngs.reduce((total, lng) => total + lng, 0) / lngs.length
+        //reduce the lats and divide by lats.length
+        const avLat = lats.reduce((total, lat) => total + lat, 0) / lats.length
 
-
+        this.setState({zoomCenter: {lng: avLng, lat: avLat}})
+        console.log('ZOOMCENTER', this.state.zoomCenter)
+      })
   }
+
 
   render(){
     if (!this.state.crawl) return null
-    console.log(this.state.crawl)
+    //console.log(this.state.crawl)
     const {
       comments,
       creator,
       description,
-      id,
       name
     } = this.state.crawl
     return(
       <main>
         <CrawlMap
           stops = {this.state.crawl.stops}
-          center={{ lat: 51.5, lng: -0.05}}
-          zoom={10.7}
+          center = {this.state.zoomCenter}
+          zoom={12.0}
         />
 
         <section className="section">
@@ -46,6 +61,10 @@ class CrawlShow extends React.Component {
             <h1 className="title is-2">{name}</h1>
             <div>{description}</div>
             <div><strong>By {creator.username}</strong></div>
+
+
+
+            
             <div className="card-content">
               {comments.map(comment => {
                 return(
