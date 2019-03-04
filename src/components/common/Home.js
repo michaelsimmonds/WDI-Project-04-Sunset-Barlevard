1510aa5f5b9925895a519c5594b48e6af4b7e455
Michael Simmonds <michaelsimmonds92@hotmail.com>
2019-03-04 18:44:51 +0000

    Have a function to determine weather a crawl is sun suitable

--- src/components/common/Home.js
+++ src/components/common/Home.js
@@ -3,6 +3,8 @@ import axios from 'axios'
 import { Link } from 'react-router-dom'
 import CrawlCard from '../crawls/CrawlCard.js'
 import Select from 'react-select'
+// import Switch from 'react-toggle-switch'
+// import {render} from 'react-dom'
 
 class Home extends React.Component{
   constructor(){
@@ -10,9 +12,40 @@ class Home extends React.Component{
 
     this.state={
       crawls: [],
-      location: []
+      location: [],
+      switched: false
     }
 
+    this.toggleSwitch = this.toggleSwitch.bind(this)
+
+  }
+
+  // get crawls with terraces
+  // map over each Crawl
+  // map over each bar in the crawl
+  // if a bar has terrace boolean true add 1 to counter
+  // at end if counter divided by bars.length is more than 0.5 the crawl is sun suitable
+  getSun() {
+    console.log(this.state.switched)
+    this.state.crawls.map(crawl => {
+      let counter = 0
+      console.log(crawl)
+      crawl.stops.forEach(stop => {
+        console.log(stop)
+        if (stop.bar.terrace === true) counter++
+      })
+      console.log(counter)
+      if ((counter / crawl.stops.length) > 0.5) console.log('sun suitable')
+      else console.log('not sun suitable')
+    })
+
+  }
+
+  toggleSwitch() {
+    this.getSun()
+    this.setState({
+      switched: !this.state.switched
+    })
   }
 
   componentDidMount() {
@@ -20,12 +53,16 @@ class Home extends React.Component{
       .then(res => this.setState({ crawls: res.data }))
   }
 
+
+
   render(){
-    console.log(this.state.crawls)
+    // console.log(this.state.crawls)
     const { stops } = this.state.crawls
     return(
       <main>
         <section className="hero is-large background">
+          <button onClick={this.toggleSwitch}>Sunshine Mode</button>
+          {this.state.switched && 'test'}
           <div className="hero-body">
             <div className="container has-text-centered">
               <h1 className="title sunset level-item">
