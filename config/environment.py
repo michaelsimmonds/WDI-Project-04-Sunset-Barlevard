796f9d6f5cbe4ec991ac8a89b77afb54628a6faf
laceswingybethler <beth.swingler@gmail.com>
2019-03-07 16:36:04 +0000

    Ready for deployment

--- config/environment.py
+++ config/environment.py
@@ -1,6 +1,6 @@
 import os
 
-secret = os.getenv('SECRET', 'shh, it\'s a secret')
+secret = os.getenv('SECRET', 'secret boulevard')
 db_uri = os.getenv('DATABASE_URL', 'postgres://localhost:5432/sunset-barlevard')
 
 #if there is secret in env file, use that one, else use the other on. os does operating system stuff.
